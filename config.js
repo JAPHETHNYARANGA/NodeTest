51030a10aab1df54f7ef25b8a10cf059c98f2ac1
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-04-04 20:33:34 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+        // should not be exposed
+          
+        host: "db4free.net",
+        user: "root@localhost",
+        password: "root",
+        database: "TestDb",
+      },
+      listPerPage: 10,
+    };
+    module.exports = config;
